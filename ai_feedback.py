3d60c37f63ff4c35a893c7025d19d554abe2ab7a
Thanmay Gowda <thanmaigowdasp12@gmail.com>
2026-06-14 20:00:06 +0530

    api changes

--- ai_feedback.py
+++ ai_feedback.py
@@ -1,9 +1,7 @@
+import streamlit as st
 import google.generativeai as genai
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = st.secrets["GEMINI_API_KEY"]
 
 genai.configure(api_key=api_key)
 
